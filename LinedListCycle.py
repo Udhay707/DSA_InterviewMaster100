a08c2cfa0b0aeb3e1a767a8dacbb5140ddf6e7b9
Udhay707 <udhaychandhar@gmail.com>
2024-12-10 23:17:05 +0000

    add linkedListCycle

---
+++ LinedListCycle.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        def eval_fast_pointer(fast_pointer):
+            return fast_pointer is None or fast_pointer.next is None
+        slow_pointer = fast_pointer = head
+        if(eval_fast_pointer(fast_pointer)):
+            return False
+        fast_pointer = head.next.next
+        while(slow_pointer != fast_pointer):
+            slow_pointer =  slow_pointer.next
+            if eval_fast_pointer(fast_pointer):
+                return False
+            fast_pointer = fast_pointer.next.next
+        return True 
+    
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None    
+
+
+
+
+
