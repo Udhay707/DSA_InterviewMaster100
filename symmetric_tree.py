58ae00eb4f3e028a38f028f691c0d7a7f28818c9
Udhay707 <udhaychandhar@gmail.com>
2024-12-21 02:12:49 +0000

    symmetric tree with BFS

---
+++ symmetric_tree.py
@@ -0,0 +1,53 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        queue = []
+        queue.append(root)
+        while(len(queue) != 0):
+            total_in_same_level = len(queue)
+            i,j = 0, total_in_same_level-1
+            while(i<=j):
+                if queue[i] == queue[j] == None:
+                    i+=1
+                    j-=1
+                    continue
+                if queue[i] == None or queue[j] == None or queue[i].val != queue[j].val:
+                    return False
+                i+=1
+                j-=1
+            for index in range(0,total_in_same_level):
+                element = queue.pop(0)
+                if element != None:
+                    queue.append(element.left)
+                    queue.append(element.right)
+                index+=1
+
+        return True
+    
+    def createTree(self, array):
+        if array == None or len(array)== 0:
+            return None
+        head = TreeNode(array.pop(0), None, None)
+        queue = [head]
+        while(len(queue)>0 and len(array) > 0):
+            total= len(queue)
+            for i in range(total):
+                element = queue.pop(0)
+                left = TreeNode(array.pop(0), None , None)
+                right = TreeNode(array.pop(0), None , None) if len(array)>0 else None
+                element.left = left
+                element.right = right
+                queue.append(left)
+                queue.append(right)
+        return head
+
+solution = Solution()
+root = solution.createTree([1,2,2,3,4,4,3])
+print(solution.isSymmetric(root))
